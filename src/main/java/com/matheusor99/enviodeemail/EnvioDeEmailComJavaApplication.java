c91041819dc355ff9fd2980f6b3e272e67063cf1
matheusor99 <matheusor99@gmail.com>
2021-02-03 19:12:43 -0300

    first commit

---
+++ src/main/java/com/matheusor99/enviodeemail/EnvioDeEmailComJavaApplication.java
@@ -0,0 +1,13 @@
+package com.matheusor99.enviodeemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvioDeEmailComJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvioDeEmailComJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheusor99/enviodeemail/EnvioDeEmailComJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusor99.enviodeemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvioDeEmailComJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
