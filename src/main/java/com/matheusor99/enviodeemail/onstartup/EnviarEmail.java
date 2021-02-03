a573a1301afbafb33fee802fd09007903661b83b
matheusor99 <matheusor99@gmail.com>
2021-02-03 19:39:23 -0300

    properties de email e enviroment

--- src/main/java/com/matheusor99/enviodeemail/onstartup/EnviarEmail.java
+++ src/main/java/com/matheusor99/enviodeemail/onstartup/EnviarEmail.java
@@ -1,11 +1,18 @@
 package com.matheusor99.enviodeemail.onstartup;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
+import org.springframework.core.env.Environment;
 
 @Service
+@PropertySource("classpath:env/email.properties")
 public class EnviarEmail {
+	
+	@Autowired
+	private Environment env;
 	
 	@EventListener(ApplicationReadyEvent.class)
 	public void enviarEmail() {
