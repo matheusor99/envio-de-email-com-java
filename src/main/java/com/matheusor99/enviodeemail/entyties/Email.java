632a12cc3837d7434d38a666f536164d81dff485
matheusor99 <matheusor99@gmail.com>
2021-02-03 20:22:52 -0300

    concluindo serviço de envio de email

--- src/main/java/com/matheusor99/enviodeemail/entyties/Email.java
+++ src/main/java/com/matheusor99/enviodeemail/entyties/Email.java
@@ -1,5 +1,7 @@
 package com.matheusor99.enviodeemail.entyties;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +13,7 @@ import lombok.NoArgsConstructor;
 public class Email {
 
 	private String assunto;
+	@Value("${MENSAGEM_EMAIL}")
 	private String mensagemEmail;
 	private String destinatario;
 	private String remetente;
--- src/main/java/com/matheusor99/enviodeemail/onstartup/EnviarEmail.java
+++ src/main/java/com/matheusor99/enviodeemail/onstartup/EnviarEmail.java
@@ -1,10 +1,15 @@
 package com.matheusor99.enviodeemail.onstartup;
 
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.SimpleEmail;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
+
+import com.matheusor99.enviodeemail.entyties.Email;
+
 import org.springframework.core.env.Environment;
 
 @Service
@@ -17,6 +22,55 @@ public class EnviarEmail {
 	@EventListener(ApplicationReadyEvent.class)
 	public void enviarEmail() {
 		
+		while(true) {
+			try {
+//				define de quanto em quanto tempo vai enviar o email, se quiser que envie só 1 vez remover o while(true) e p Thread.sleep
+				Thread.sleep(3000);
+				
+				Email email = new Email();
+				
+				email.setAssunto(this.env.getProperty("email.smtp.useremail"));
+				email.setRemetente(this.env.getProperty("email.smtp.useremail"));
+				email.setDestinatario(this.env.getProperty("email.smtp.destinatario"));
+				
+				this.emailConfigAndSend(email);
+				
+				System.out.println("email enviado");
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+		}
+	}
+	
+	public void emailConfigAndSend(Email emailInfos) {
+//		configuração para enviar email
+		SimpleEmail email = new SimpleEmail();
+		email.setHostName(this.env.getProperty("email.smtp.hostname"));
+		email.setSmtpPort(this.env.getProperty("email.smtp.port", Integer.class));
+		email.setAuthenticator(
+				new DefaultAuthenticator(
+						this.env.getProperty("email.smtp.useremail"),
+						this.env.getProperty("email.smtp.password")));
+		email.setSSLOnConnect(true);
+		
+//		enviando o email
+		try {
+		
+//			remetente do email
+			email.setFrom(emailInfos.getRemetente());
+//			destinatario do email
+			email.addTo(emailInfos.getDestinatario());
+//			assunto do email
+			email.setSubject(emailInfos.getAssunto());
+//			mensagem do email
+			email.setMsg(emailInfos.getMensagemEmail());
+			
+			email.send();
+
+		} catch (Exception e) {
+			System.out.println("erro: "+ e.getMessage() +"/n");
+			e.printStackTrace();
+		}
 	}
 
 }
